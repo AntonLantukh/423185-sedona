19749900f81d7f36c5e5d749b323615b2919c418
AntonLantukh <lantukh@bk.ru>
2017-05-14 13:06:18 +0300

    JS continue

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 var link = document.querySelector(".search-name");
 var popup = document.querySelector(".search-basis");
+var form = popup.querySelector("form");
+var datein = popup.querySelector(".date-in");
+var dateout = popup.querySelector(".date-out");
+var adults = popup.querySelector(".adults");
+var children = popup.querySelector(".children");
 
 link.addEventListener("click", function(evt){
   evt.preventDefault();
-  popup.classList.add("model-content-show");
+  popup.classList.toggle("model-content-show");
+});
+
+form.addEventListener("submit", function(evt){
+  if (!datein.value || !dateout.value || !adults.value || !children.value)
+   evt.preventDefault();
+   console.log("Нет данных");
 });
